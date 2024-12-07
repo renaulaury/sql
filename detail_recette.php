f9a559128f0f0baa4729f5f6a3692ec9714e708a
Lily <112404773+Belywen@users.noreply.github.com>
2024-12-07 14:15:12 +0100

    maj

--- detail_recette.php
+++ detail_recette.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./styles.css" rel="stylesheet"/>
+    <link href="./styles.css" rel="stylesheet" />
     <title>Détail recette</title>
 </head>
+
 <body>
-    <header> 
+    <header>
         <nav>
             <!-- <a href="./detail_recette.php">Détail recette</a> -->
             <a href="./recettes.php">Listing recettes</a>
@@ -18,4 +20,5 @@
         <h1>Détail de la recette</h1>
     </main>
 </body>
+
 </html>
